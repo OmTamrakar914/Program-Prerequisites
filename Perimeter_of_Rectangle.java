aeda1c670895e544cc2c7ac934bd932b9884297c
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-08 11:19:24 +0530

    7 problem

---
+++ Perimeter_of_Rectangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Perimeter_of_Rectangle{
+
+ public static void main(String []args){
+
+  Scanner sc = new Scanner(System.in);
+
+  double length = sc.nextInt();
+  double width = sc.nextInt();
+  
+  double perimeter = 2 * (length + width);
+
+  
+   System.out.println("Perimeter of Rectangle = "+perimeter);
+
+   }
+}
