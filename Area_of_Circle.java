cd53f0561b2ca140a13b139405bcb52aa58921fb
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-08 10:55:56 +0530

    4 problem

---
+++ Area_of_Circle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Area_of_Circle{
+
+ public static void main(String []args){
+
+  Scanner sc = new Scanner(System.in);
+
+  double radius = sc.nextInt();
+  double area = area = 3.14 * radius*radius;
+
+  
+   System.out.println("Area of Circle = "+area);
+
+   }
+}
